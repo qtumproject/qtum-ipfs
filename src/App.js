0f65b308e7dfe1a7a646aa71abae189dbc4ec032
DaniloShan <awpszq@gmail.com>
2018-11-02 16:05:54 +0800

    add style

--- src/App.js
+++ src/App.js
@@ -2,14 +2,61 @@
 import React from 'react'
 import ipfsAPI from 'ipfs-api'
 import { Qtum } from 'qtumjs'
+import Button from '@material-ui/core/Button'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableRow from '@material-ui/core/TableRow'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import { withStyles } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Grid from '@material-ui/core/Grid'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { blue } from '@material-ui/core/colors'
 import repo from './solar.development.json'
 
+const theme = createMuiTheme({
+    typography: {
+        useNextVariants: true
+    },
+    palette: {
+        primary: {
+            light: blue[300],
+            main: blue[500],
+            dark: blue[700],
+            contrastText: '#fff'
+        }
+    }
+})
+
+const styles = (theme) => ({
+    container: {
+        width: '960px',
+        margin: '0 auto'
+    },
+    input: {
+        display: 'none'
+    },
+    button: {
+        margin: theme.spacing.unit
+    },
+    snackbar: {
+        margin: theme.spacing.unit
+    },
+    fileHash: {
+        color: '#fff',
+        underline: 'none'
+    }
+})
+
+@withStyles(styles)
 export default class App extends React.Component {
 
     state = {
         added_file_hash: null,
         files_in_contract: [],
-        addressArray: []
+        addressArray: [],
+        loading: false
     }
 
     //ipfs initial
@@ -37,7 +84,7 @@ export default class App extends React.Component {
         let sendStr = ''
         let fileResult = []
         const buffer = Buffer.from(reader.result)
-
+        this.setState({ loading: true })
         //get each record in the contract
         const getIndexRes = await this.contract.call("getIndex")
         const recordNum = getIndexRes["outputs"][0]["words"][0]
@@ -65,7 +112,7 @@ export default class App extends React.Component {
             console.log(response)
             ipfsId = response[0].hash
             console.log(ipfsId)
-            this.setState({ added_file_hash: ipfsId })
+            this.setState({ added_file_hash: ipfsId, loading: false })
             //add  a record to the contract of qtum
             fromAddr = 'qd8KM6Zb6TdAes1u9opK4Cr6YbhLKiQ6U9'
             let myDate = new Date()
@@ -85,34 +132,68 @@ export default class App extends React.Component {
     }
 
     render() {
-        const { added_file_hash, files_in_contract, addressArray } = this.state
+        const { added_file_hash, files_in_contract, addressArray, loading } = this.state
         const style = { width: '50%' }
+        const { classes } = this.props
         return (
-            <div>
-                <form id='captureMedia' onSubmit={this.handleSubmit}>
-                    <input type='file' onChange={this.captureFile} />
-                </form>
-                <div>
-                    <a
-                        target='_blank'
-                        href={'https://ipfs.io/ipfs/' + added_file_hash}
-                    >
-                        {added_file_hash}
-                    </a>
-                </div>
-                <table>
-                    <tbody>
+            <MuiThemeProvider theme={theme}>
+                <Grid className={classes.container} container spacing={24}>
+                    <CssBaseline />
+                    <Grid item xs={4}>
+                        <input
+                            className={classes.input}
+                            id="outlined-button-file"
+                            type="file"
+                            onChange={this.captureFile}
+                        />
+                        <label htmlFor="outlined-button-file">
+                            {
+                                loading ? <CircularProgress className={classes.progress} />
+                                    :
+                                    <Button
+                                        variant="outlined"
+                                        component="span"
+                                        color="primary"
+                                        className={classes.button}
+                                    >
+                                        Upload
+                                    </Button>
+                            }
+                        </label>
+                    </Grid>
+                    <Grid item xs={8}>
                         {
-                            files_in_contract.map((file, index) => (
-                                <tr key={index}>
-                                    <td style={style}>{file}</td>
-                                    <td style={style}>{addressArray[index]}</td>
-                                </tr>
-                            ))
+                            added_file_hash ?
+                                <div>
+                                    <Button
+                                        variant="outlined"
+                                        component="a"
+                                        color="success"
+                                        className={classes.button}
+                                        target='_blank'
+                                        href={'https://ipfs.io/ipfs/' + added_file_hash}
+                                    >
+                                        {added_file_hash}
+                                    </Button>
+                                </div>
+                                :
+                                null
                         }
-                    </tbody>
-                </table>
-            </div>
+                    </Grid>
+                    <Table>
+                        <TableBody>
+                            {
+                                files_in_contract.map((file, index) => (
+                                    <TableRow key={index}>
+                                        <TableCell style={style}>{file}</TableCell>
+                                        <TableCell style={style}>{addressArray[index]}</TableCell>
+                                    </TableRow>
+                                ))
+                            }
+                        </TableBody>
+                    </Table>
+                </Grid>
+            </MuiThemeProvider>
         )
     }
 }
