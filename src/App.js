ae45e470f6e9eea081073c05298668293e182ac1
zhengyi <zhengyi.bupt@qq.com>
2018-11-13 20:54:23 +0800

    modify

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 'use strict'
 import React from 'react'
 import ipfsAPI from 'ipfs-api'
-import { Qtum } from 'qtumjs'
+import {Qtum} from 'qtumjs'
 import Button from '@material-ui/core/Button'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
 import TableRow from '@material-ui/core/TableRow'
 import CircularProgress from '@material-ui/core/CircularProgress'
-import { withStyles } from '@material-ui/core/styles'
+import {withStyles} from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Grid from '@material-ui/core/Grid'
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
-import { blue } from '@material-ui/core/colors'
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles'
+import {blue} from '@material-ui/core/colors'
 import repo from './solar.development.json'
 
 const theme = createMuiTheme({
@@ -54,27 +54,52 @@ const styles = (theme) => ({
     }
 })
 
-@withStyles(styles)
-export default class App extends React.Component {
-
+@ withStyles(styles)
+export
+default class App extends React.Component {
     state = {
-        added_file_hash: null,
-        files_in_contract: [],
-        addressArray: [],
+        addedFileHash: null,
+        filesInContract: [],
         loading: false
     }
 
-    //ipfs initial
-    // ipfsApi = ipfsAPI('localhost', '5001')
-    ipfsApi = ipfsAPI('ipfs.infura.io', '5001', { protocol: 'https' })
-
-    //qtum initial
-    contract = (new Qtum('http://atx:atx@192.168.3.25:8010', repo)).contract('ipfs.sol')
-
-    fileName = ''
+    //init IPFS
+    ipfsApi = ipfsAPI('ipfs.infura.io', '5001', {protocol: 'https'})
+    //init Qtum
+    contract = (new Qtum('http://atx:atx@127.0.0.1:8010', repo)).contract('ipfs.sol')
 
     componentDidMount() {
         this.getContractInfo()
+    }
+
+    getContractInfo = async() => {
+        let fileResult = []
+
+        // get record number
+        const getIndexRes = await this.contract.call("getIndex")
+        const recordNum = getIndexRes["outputs"][0].toNumber()
+        console.log("How many records in the contract: ", recordNum)
+
+        // get each record
+        const getRecordRes = await Promise.all(Array.from({
+            length: recordNum
+        }, (item, i) => this.contract.call("get", [i])))
+        getRecordRes.forEach((recordRes) => {
+            const oneRecord = recordRes['outputs'][0].split("\t")
+            if (oneRecord.length == 3) {
+                fileResult.push({
+                    dateTime: new Date(parseInt(oneRecord[0])).toLocaleString(),
+                    fileName: oneRecord[1],
+                    ipfsAddress: oneRecord[2]
+                })
+            }
+        })
+
+        // Show result
+        console.log("Records:", fileResult)
+        this.setState({
+            filesInContract: fileResult,
+        })
     }
 
     captureFile = (event) => {
@@ -82,66 +107,49 @@ export default class App extends React.Component {
         event.preventDefault()
         const file = event.target.files[0]
         const reader = new window.FileReader()
-        this.fileName = event.target.files[0]['name']
-        reader.onloadend = () => this.saveToIpfs(reader)
+        const fileName = event.target.files[0]['name']
+        reader.onloadend = () => this.saveFile(fileName, reader)
         reader.readAsArrayBuffer(file)
     }
 
-    getContractInfo = async () => {
-        this.fileResult = []
-
-        //get each record in the contract
-        const getIndexRes = await this.contract.call("getIndex")
-        const recordNum = getIndexRes["outputs"][0]["words"][0]
-        console.log("how many records in the contract:", recordNum)
-
-        const getRecordRes = await Promise.all(Array.from({ length: recordNum }, (item, i) => this.contract.call("getRecord", [i])))
-        getRecordRes.forEach((recordRes) => {
-            const oneRecord = recordRes['outputs'][0].toString()
-            this.fileResult.push(oneRecord)
-        })
-
-        //get the sender address array
-        const getAddressArrRes = await this.contract.call("getAddressArr")
+    saveFile = async(fileName, reader) => {
         this.setState({
-            addressArray: getAddressArrRes['outputs'][0],
-            files_in_contract: this.fileResult,
+            loading: true
         })
 
-        console.log("all send address array :", this.state.addressArray)
-        console.log("file result:", this.fileResult)
-    }
-
-    saveToIpfs = async (reader) => {
-        let ipfsId = ''
-        let fromAddr = ''
-        let sendStr = ''
+        // Add file to IPFS
+        console.log('Adding file to IPFS: ', fileName)
         const buffer = Buffer.from(reader.result)
-        this.setState({ loading: true })
+        const response = await this.ipfsApi.add(buffer, {
+            progress: (prog) => console.log(`IPFS received: ${prog}`)
+        })
+        console.log('IPFS response: ', response)
+        this.setState({
+            addedFileHash: response[0].hash
+        })
 
-        //add a file to ipfs
+        // Add file to Qtum
+        console.log('Adding file to Qtum')
+        const sendStr = (new Date()).valueOf() + "\t" + fileName + "\t" + response[0].hash
+        const sendRes = await this.contract.send("add", [sendStr], {
+            gasLimit: 1000000
+        })
         try {
-            const response = await this.ipfsApi.add(buffer, { progress: (prog) => console.log(`received: ${prog}`) })
-            console.log(response)
-            ipfsId = response[0].hash
-            console.log(ipfsId)
-            this.setState({ added_file_hash: ipfsId, loading: false })
-            //add  a record to the contract of qtum
-            fromAddr = 'qd8KM6Zb6TdAes1u9opK4Cr6YbhLKiQ6U9'
-            let myDate = new Date()
-            sendStr = this.fileName + '__' + ipfsId + '__' + Date.parse(myDate) + '__' + myDate.toLocaleString()
-            const sendRes = await this.contract.send("sendHash", [sendStr], { senderAddress: fromAddr, })
-
-            console.log("send response fee:", sendRes['fee'])
-            console.log("send response txid:", sendRes['txid'])
-            console.log("send response confirmations:", sendRes['confirmations'])
+            await sendRes.confirm(1)
         } catch (e) {
-            console.error(e)
+            console.log(e)
         }
+
+        // Show result
+        console.log("Send response:", sendRes)
+        this.getContractInfo()
+        this.setState({
+            loading: false
+        })
     }
 
     render() {
-        const { added_file_hash, files_in_contract, addressArray, loading } = this.state
+        const { addedFileHash, filesInContract, addressArray, loading } = this.state
         const { classes } = this.props
         return (
             <MuiThemeProvider theme={theme}>
@@ -171,7 +179,7 @@ export default class App extends React.Component {
                     </Grid>
                     <Grid item xs={8}>
                         {
-                            added_file_hash ?
+                            addedFileHash ?
                                 <div>
                                     <Button
                                         variant="outlined"
@@ -179,9 +187,9 @@ export default class App extends React.Component {
                                         color="primary"
                                         className={classes.button}
                                         target='_blank'
-                                        href={'https://ipfs.infura.io/ipfs/' + added_file_hash}
+                                        href={'https://ipfs.infura.io/ipfs/' + addedFileHash}
                                     >
-                                        {added_file_hash}
+                                        {addedFileHash}
                                     </Button>
                                 </div>
                                 :
@@ -191,12 +199,18 @@ export default class App extends React.Component {
                     <Table>
                         <TableBody>
                             {
-                                files_in_contract.map((file, index) => (
+                                filesInContract.map((file, index) => (
                                     <TableRow key={index}>
-                                        {
-                                            (file.split('__').length === 4 ? file.split('__') : (file + '__').split('__')).map((str, i) => <TableCell key={i} className={classes.tableCell}>{str}</TableCell>)
-                                        }
-                                        <TableCell className={classes.tableCell}>{addressArray[index]}</TableCell>
+                                        <TableCell className={classes.tableCell}>{file['dateTime']}</TableCell>
+                                        <TableCell className={classes.tableCell}>{file['fileName']}</TableCell>
+                                        <TableCell className={classes.tableCell}>
+                                            <a
+                                                href={'https://ipfs.infura.io/ipfs/' + file['ipfsAddress']}
+                                                target='_blank'
+                                            >
+                                                {file['ipfsAddress']}
+                                            </a>
+                                        </TableCell>
                                     </TableRow>
                                 ))
                             }
--- src/ipfs.sol
+++ src/ipfs.sol
@@ -1,25 +1,16 @@
 pragma solidity ^0.4.24;
-contract ipfs{
- string[] records;
- address[] addr;
- uint256 index = 0;
- 
- function sendHash(string recordData) public {
-     records.push(recordData);
-     addr.push(msg.sender);
-     index++;
- }
- 
- function getIndex() public view returns (uint256 resIndex){
-     resIndex = index;
-     return resIndex;
- }
- function getRecord(uint256 _index) public view returns (string oneRecord) {
-   oneRecord = records[_index];
-   return oneRecord;
- }
- function getAddressArr() public view returns (address[] addrArr){
-     addrArr = addr;
-     return addrArr;
- }
+contract IpfsRecord {
+    string[] records;
+
+    function add(string data) external {
+        records.push(data);
+    }
+    
+    function getIndex() public view returns (uint) {
+        return records.length;
+    }
+    
+    function get(uint index) public view returns (string) {
+        return records[index];
+    }
 }
